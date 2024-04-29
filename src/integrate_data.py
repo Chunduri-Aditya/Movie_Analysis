c667015140a5de7338af259e2ffb95f2c91fec0a
Aditya Chunduri <51287255+Chunduri-Aditya@users.noreply.github.com>
2024-04-28 19:55:14 -0700

    Update integrate_data.py

--- src/integrate_data.py
+++ src/integrate_data.py
@@ -3,20 +3,29 @@ Integrates all the data into a format that can be easily analyzed
 This will probably take the form of merging (joining) several Pandas
 DataFrames, or issuing SQL queries over tables in a relational DB.
 
-Author: < Student Name >
+Author: < Chunduri Aditya >
 """
 import pandas as pd
+import os
 
-def merge_datasets(imdb_movies_list, movie_reviews, movies_metadata):
+def merge_data(imdb_movies_list, movie_reviews, movies_metadata):
+    # Merge dataframes
     merged_reviews = pd.merge(imdb_movies_list, movie_reviews, on="movie_title", how="inner")
     final_merged_data = pd.merge(merged_reviews, movies_metadata, on="movie_title", how="inner")
     return final_merged_data
 
-def prepare_final_data(final_merged_data):
-    columns_to_keep = [
-        'movie_title', 'rating', 'genres', 'budget', 'revenue', 
-        'review_text', 'year', 'runtime', 'vote_average', 'vote_count', 'cleaned_genres'
-    ]
-    final_data = final_merged_data[columns_to_keep]
-    final_data.to_csv('/data/processed/final_data.csv', index=False)
-    return final_data
+def main():
+    # Read data from input files
+    imdb_movies_list = pd.read_csv('data/raw/imdb_movies_list.csv')
+    movie_reviews = pd.read_csv('data/raw/movie_reviews.csv')
+    movies_metadata = pd.read_csv('data/raw/movies_metadata_kaggle.csv', low_memory=False)
+    
+    # Merge data
+    integrated_data = merge_data(imdb_movies_list, movie_reviews, movies_metadata)
+    
+    # Save integrated data
+    integrated_data.to_csv('data/processed/final_data.csv', index=False)
+
+if __name__ == "__main__":
+    main()
+
