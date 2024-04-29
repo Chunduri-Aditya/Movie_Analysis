479d34efa21f2aa36b382e0f347094a0d6d29815
Aditya Chunduri <51287255+Chunduri-Aditya@users.noreply.github.com>
2024-04-28 19:55:41 -0700

    Update get_data.py

--- src/get_data.py
+++ src/get_data.py
@@ -8,3 +8,16 @@ from src.utils.helper import helper_function_name
 
 result = helper_function_name(argument=False)
 print(result)
+
+import pandas as pd
+
+def main():
+    # Read data from existing CSV files
+    imdb_data = pd.read_csv('data/raw/imdb_movies_list.csv')
+    rotten_tomatoes_data = pd.read_csv('data/raw/movie_reviews.csv')
+    movies_metadata = pd.read_csv('data/raw/movies_metadata_kaggle.csv', low_memory=False)
+
+    # Your further processing code goes here
+
+if __name__ == "__main__":
+    main()
