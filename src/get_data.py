d4c89786947fbdc2c443319898977febd0688951
Aditya Chunduri <51287255+Chunduri-Aditya@users.noreply.github.com>
2024-04-30 12:38:42 -0700

    get_data.py

--- src/get_data.py
+++ src/get_data.py
@@ -11,14 +11,10 @@ import os
 def main():
     data_dir = 'data/raw'
     # Ensure the data directory exists
-    os.makedirs(data_dir, exist_ok=True)
-
     # Read data from existing CSV files
     imdb_data = pd.read_csv(os.path.join(data_dir, 'imdb_movies_list.csv'))
     rotten_tomatoes_data = pd.read_csv(os.path.join(data_dir, 'movie_reviews.csv'))
     movies_metadata = pd.read_csv(os.path.join(data_dir, 'movies_metadata_kaggle.csv'), low_memory=False)
-
-    # Further processing code goes here (optional)
 
 if __name__ == "__main__":
     main()
